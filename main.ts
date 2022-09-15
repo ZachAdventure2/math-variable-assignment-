c735b8b5df247a0aa920a3fd9365860f7ee3e2fb
ZachAdventure2 <113042161+ZachAdventure2@users.noreply.github.com>
2022-09-15 11:14:44 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,10 +2,22 @@ input.onButtonPressed(Button.A, function () {
     NUMB += 1
     basic.showNumber(NUMB)
 })
+input.onButtonPressed(Button.AB, function () {
+    Balls = 3
+    basic.showNumber(Balls)
+    Strikes = 4
+    basic.showNumber(Strikes)
+})
 input.onButtonPressed(Button.B, function () {
     NUMB += 2
     basic.showNumber(NUMB)
 })
+input.onGesture(Gesture.Shake, function () {
+    NUMB = 0
+    basic.showNumber(NUMB)
+})
+let Strikes = 0
+let Balls = 0
 let NUMB = 0
 basic.showIcon(IconNames.Skull)
 let Pic = 0
